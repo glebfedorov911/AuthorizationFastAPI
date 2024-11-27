caf0e6ea8e7ce0ea94d426aaedefb848a306ed71
Gleb <fedorov22134@gmail.com>
2024-11-28 00:34:49 +0300

    for view

--- app/v1/auth/views.py
+++ app/v1/auth/views.py
@@ -4,12 +4,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import UserCreateScheme
 
-from .TokenWorker import AccessTokenCreater
+from .TokenWorker import AccessTokenCreater, RefreshTokenCreater
 
 
 router = APIRouter(prefix="/auth", tags=["Authorization"])
 
-@router.post("/sign_up")
-async def sign_up():
-    tkn = AccessTokenCreater()
-    print(tkn.get_token({"id": 1, "username": "gleb", "status": "active"}))
+@router.post("/check_tkn_a")
+async def check_tkn_a():
+    tkn: AccessTokenCreater = AccessTokenCreater()
+    token = (tkn.get_token({"id": 1, "username": "gleb", "status": "active"}))
+    print(token)
+    return tkn.read_token(token)
+
+@router.post("/check_tkn_r")
+async def check_tkn_r():
+    tkn: RefreshTokenCreater = RefreshTokenCreater()
+    token = (tkn.get_token({"id": 1, "username": "gleb", "status": "active"}))
+    print(token)
+    return tkn.read_token(token)

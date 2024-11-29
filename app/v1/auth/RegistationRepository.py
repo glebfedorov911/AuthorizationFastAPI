aeb257112af067dd6747ec00dea0044ad7dc7fd1
Gleb <fedorov22134@gmail.com>
2024-11-30 01:07:06 +0300

    delete @property

--- app/v1/auth/RegistationRepository.py
+++ app/v1/auth/RegistationRepository.py
@@ -9,9 +9,8 @@ class RegistrationRepository:
     def __init__(self, session: AsyncSession, userIn: UserCreateScheme):
         self.session = session
         self.userIn = userIn
-        self.passwordHasher = self.createPasswordHasher
+        self.passwordHasher = self.createPasswordHasher()
 
-    @property
     def createPasswordHasher(self):
         password = self.userIn.password
         return PasswordHasher(password=password)
